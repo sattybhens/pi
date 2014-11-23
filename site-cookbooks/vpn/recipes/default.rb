d2737ef237a296f942b0de2d60e3506b7ecc95cd
Satty Bhens <satty_bhens@mckinsey.com>
2014-11-23 11:20:25 -0500

    updated for openvpn setup with VyperVpn

--- site-cookbooks/vpn/recipes/default.rb
+++ site-cookbooks/vpn/recipes/default.rb
@@ -10,3 +10,18 @@
 include_recipe "apt"
 
 package "network-manager-openvpn" do action :install end
+
+template "/etc/openvpn/client.conf" do
+  source "client.conf.erb"
+  action :create 
+end
+
+template "/home/pi/auth.txt" do
+  source "auth.txt.erb"
+  action :create 
+end
+
+remote_file "/etc/openvpn/ca.vyprvpn.com.crt" do
+  source "https://www.goldenfrog.com/downloads/ca.vyprvpn.com.crt"
+end
+
